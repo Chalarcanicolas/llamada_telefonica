3701c34338d8c2cca731a4cb844e769bf830c22c
Chalarcanicolas <nicolaschalarca@colegioguanenta.edu.co>
2023-02-17 09:49:46 -0500

    Se agregan los programas

---
+++ llamada_telefonica.py
@@ -0,0 +1,23 @@
+# llamada telefonica
+
+# input 
+
+X = int(input("digite los minutos"))
+
+#Processing
+
+if X < 4:
+    Y = 300
+
+else:
+    Y = (X * 50) + 300 - 150
+
+
+if Y > 999:
+    msj = "mil"
+
+else:
+    msj = "pesos"
+
+#output
+print ("los gastos totales son " + str(Y) + " " + msj)
